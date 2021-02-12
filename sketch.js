574c106d530fac0b7c1d69f3834b71681a0e0690
Aashmi12 <73058981+Aashmi12@users.noreply.github.com>
2021-02-12 20:01:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+var database;
+var backgroundImg;
+var gameState = 0;
+var playerCount = 0;
+var allPlayers;
+var score = 0;
+var player, form,game;
+var player1,player2;
+var players;
+var fruits;
+var fruitGroup;
+var fruit1Img, fruit2Img, fruit3Img, fruit4Img, fruit5Img;
+var playerImg;
+var player1score = 0;
+var player2score = 0;
+
+function preload(){
+  backgroundImg = loadImage("images/jungle.jpg");
+  playerImg = loadImage("images/basket2.png");
+  fruit1Img = loadImage("images/apple2.png");
+  fruit2Img = loadImage("images/banana2.png");
+  fruit3Img = loadImage("images/melon2.png");
+  fruit4Img = loadImage("images/orange2.png");
+  fruit5Img = loadImage("images/pineapple2.png");
+
+  fruitGroup = new Group();
+}
+function setup() {
+  createCanvas(1000, 600);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+  
+}
+
+function draw() {
+  background(backgroundImg);
+
+  if (playerCount === 2) {
+    game.update(1);
+  }
+  if (gameState === 1) {
+    clear(); 
+    game.play();
+  }
+  if (gameState === 2) {
+   game.end();
+  }
+
+}
